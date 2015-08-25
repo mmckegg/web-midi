cdb7186b919c48fb795a1e25dde40687948bbbb9
Matt McKegg <matt@wetsand.co.nz>
2015-08-25 13:14:25 +1200

    fix watchPortNames initial populate

--- index.js
+++ index.js
@@ -173,6 +173,7 @@ module.exports.watchPortNames = function(listener) {
     if (!err) {
       midi = m
       midi.addEventListener('statechange', handleEvent) 
+      listener(getPortNames(midi))
     }
   })
 
